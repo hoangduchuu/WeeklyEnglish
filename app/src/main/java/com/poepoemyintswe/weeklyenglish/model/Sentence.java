49f36172ea32d261aaa9ce12c71b6fe42148b524
Poe Poe <poepoe88@gmail.com>
2015-01-13 22:42:10 +0630

    Sentence Model Class is added

---
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/model/Sentence.java
@@ -0,0 +1,10 @@
+package com.poepoemyintswe.weeklyenglish.model;
+
+/**
+ * Created by poepoe on 13/1/15.
+ */
+public class Sentence {
+    public int id;
+    public String english;
+    public String burmese;
+}
