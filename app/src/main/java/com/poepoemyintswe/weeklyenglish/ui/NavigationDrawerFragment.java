cf4d85110d7a0535636d090cd983d2a5ed8b5eec
Poe Poe <poepoe88@gmail.com>
2015-01-13 22:33:26 +0630

    Navigation ListView Divider

--- app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/NavigationDrawerFragment.java
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/NavigationDrawerFragment.java
@@ -80,7 +80,7 @@ public class NavigationDrawerFragment extends Fragment {
       }
     });
     mDrawerListView.setAdapter(new ArrayAdapter<>(getActivity(),
-                                                     android.R.layout.simple_list_item_activated_1,
+                                                     android.R.layout.simple_list_item_1,
                                                      android.R.id.text1,
                                                      getResources().getStringArray(R.array.nav_array)));
     mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
