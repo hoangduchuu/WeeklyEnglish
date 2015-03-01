b656b376f172ad4de990c532df77fdbd2cf3f37d
Poe Poe <poepoe88@gmail.com>
2015-03-01 20:57:54 +0630

    Lesson model added

---
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/model/Lesson.java
@@ -0,0 +1,11 @@
+package com.poepoemyintswe.weeklyenglish.model;
+
+/**
+ * Created by poepoe on 1/3/15.
+ */
+public class Lesson {
+  public String id;
+  public String name;
+  public String url;
+  public String explanation;
+}
--- app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/MainActivity.java
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/MainActivity.java
@@ -6,55 +6,50 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-
 import com.poepoemyintswe.weeklyenglish.R;
 import com.poepoemyintswe.weeklyenglish.ui.fragment.NavigationDrawerFragment;
 import com.poepoemyintswe.weeklyenglish.ui.fragment.RandomFragment;
 
-public class MainActivity extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends ActionBarActivity
+    implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-    private Fragment fragment = null;
+  private Fragment fragment = null;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        NavigationDrawerFragment mNavigationDrawerFragment =
-                (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+    NavigationDrawerFragment mNavigationDrawerFragment =
+        (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(
+            R.id.navigation_drawer);
 
-        mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+    mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
+        (DrawerLayout) findViewById(R.id.drawer_layout));
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+    fragment = RandomFragment.getInstance();
+  }
 
+  @Override
+  public void onNavigationDrawerItemSelected(int position) {
+    switch (position) {
+      case 0:
         fragment = RandomFragment.getInstance();
+        break;
+      case 1:
+        fragment = RandomFragment.getInstance();
+        break;
+      case 2:
+        fragment = RandomFragment.getInstance();
+        break;
     }
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        switch (position) {
-            case 0:
-                fragment = RandomFragment.getInstance();
-                break;
-            case 1:
-                fragment = RandomFragment.getInstance();
-                break;
-            case 2:
-                fragment = RandomFragment.getInstance();
-                break;
-        }
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
-            }
-        }, 300);
-    }
-
-
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+      }
+    }, 300);
+  }
 }
