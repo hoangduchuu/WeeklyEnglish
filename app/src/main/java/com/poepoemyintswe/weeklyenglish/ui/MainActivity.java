9641bf1def54580aa1f0cb707ed41bef3c9256a4
Poe Poe <poepoe88@gmail.com>
2015-03-03 22:04:19 +0630

    add fragment

--- app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/MainActivity.java
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/MainActivity.java
@@ -1,55 +1,21 @@
 package com.poepoemyintswe.weeklyenglish.ui;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import com.poepoemyintswe.weeklyenglish.R;
-import com.poepoemyintswe.weeklyenglish.ui.fragment.NavigationDrawerFragment;
 import com.poepoemyintswe.weeklyenglish.ui.fragment.RandomFragment;
 
-public class MainActivity extends ActionBarActivity
-    implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-
-  private Fragment fragment = null;
+public class MainActivity extends ActionBarActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    NavigationDrawerFragment mNavigationDrawerFragment =
-        (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(
-            R.id.navigation_drawer);
-
-    mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
-        (DrawerLayout) findViewById(R.id.drawer_layout));
-
-    fragment = RandomFragment.getInstance();
-  }
-
-  @Override
-  public void onNavigationDrawerItemSelected(int position) {
-    switch (position) {
-      case 0:
-        fragment = RandomFragment.getInstance();
-        break;
-      case 1:
-        fragment = RandomFragment.getInstance();
-        break;
-      case 2:
-        fragment = RandomFragment.getInstance();
-        break;
+    
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction()
+          .add(R.id.container, RandomFragment.getInstance())
+          .commit();
     }
-
-    new Handler().postDelayed(new Runnable() {
-      @Override
-      public void run() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
-      }
-    }, 300);
   }
 }
