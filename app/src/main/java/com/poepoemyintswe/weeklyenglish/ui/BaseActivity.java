629baf3dec01c91f6ffdea74ecd35d09d95b759b
Poe Poe <poepoe88@gmail.com>
2015-03-08 01:47:35 +0630

    something about styles

--- app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/BaseActivity.java
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.poepoemyintswe.weeklyenglish.ui;
 
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -8,6 +9,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
+import android.widget.TextView;
 import com.poepoemyintswe.weeklyenglish.R;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
@@ -15,18 +17,20 @@ import com.readystatesoftware.systembartint.SystemBarTintManager;
  * Created by poepoe on 7/3/15.
  */
 public abstract class BaseActivity extends ActionBarActivity {
-  private Toolbar toolbar;
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(getLayoutResource());
-    toolbar = (Toolbar) findViewById(R.id.toolbar);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    TextView title = (TextView) findViewById(R.id.actionbar_title);
 
     if (toolbar != null) {
       setSupportActionBar(toolbar);
       getSupportActionBar().setDisplayHomeAsUpEnabled(getHomeUpEnabled());
-      getSupportActionBar().setTitle(getCustomTitle());
+      getSupportActionBar().setTitle("");
+      title.setText(getCustomTitle());
+      Typeface typeface = Typeface.createFromAsset(getAssets(), "good_times_rg.ttf");
+      title.setTypeface(typeface);
     }
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
       SystemBarTintManager tintManager = new SystemBarTintManager(this);
