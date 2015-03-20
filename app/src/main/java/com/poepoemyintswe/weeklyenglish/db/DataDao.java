b3cd46e10b96a00c38286378ff68c604b1516b79
Poe Poe <poepoe88@gmail.com>
2015-03-20 14:23:23 +0800

    GSON converter for RestAdapter

--- app/src/main/java/com/poepoemyintswe/weeklyenglish/db/DataDao.java
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/db/DataDao.java
@@ -1,6 +1,5 @@
 package com.poepoemyintswe.weeklyenglish.db;
 
-import com.google.gson.annotations.SerializedName;
 import io.realm.RealmList;
 import io.realm.RealmObject;
 
@@ -8,6 +7,5 @@ import io.realm.RealmObject;
  * Created by poepoe on 20/3/15.
  */
 public class DataDao extends RealmObject {
-
-  @SerializedName("lessons") private RealmList<LessonDao> lessons;
+  private RealmList<LessonDao> lessons;
 }
--- app/src/main/java/com/poepoemyintswe/weeklyenglish/utils/CustomRestAdapter.java
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/utils/CustomRestAdapter.java
@@ -3,12 +3,18 @@ package com.poepoemyintswe.weeklyenglish.utils;
 import android.app.Activity;
 import android.content.Context;
 import android.util.Base64;
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.poepoemyintswe.weeklyenglish.BuildConfig;
 import com.poepoemyintswe.weeklyenglish.Config;
 import com.squareup.okhttp.OkHttpClient;
+import io.realm.RealmObject;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
+import retrofit.converter.GsonConverter;
 
 import static com.poepoemyintswe.weeklyenglish.Config.PASSWORD;
 import static com.poepoemyintswe.weeklyenglish.Config.USER_NAME;
@@ -34,6 +40,16 @@ public class CustomRestAdapter {
   }
 
   public RestAdapter normalRestAdapter() {
+    Gson gson = new GsonBuilder().setExclusionStrategies(new ExclusionStrategy() {
+      @Override public boolean shouldSkipField(FieldAttributes f) {
+        return f.getDeclaringClass().equals(RealmObject.class);
+      }
+
+      @Override public boolean shouldSkipClass(Class<?> clazz) {
+        return false;
+      }
+    }).create();
+
     final String credentials = USER_NAME + ":" + PASSWORD;
     RequestInterceptor requestInterceptor = new RequestInterceptor() {
       @Override public void intercept(RequestFacade request) {
@@ -52,6 +68,7 @@ public class CustomRestAdapter {
           .setClient(new OkClient(okHttpClient))
           .setErrorHandler(new RetrofitErrorHandler((Activity) mContext))
           .setRequestInterceptor(requestInterceptor)
+          .setConverter(new GsonConverter(gson))
           .build();
     } else
 
@@ -60,6 +77,7 @@ public class CustomRestAdapter {
           .setClient(new OkClient(okHttpClient))
           .setErrorHandler(new RetrofitErrorHandler((Activity) mContext))
           .setRequestInterceptor(requestInterceptor)
+          .setConverter(new GsonConverter(gson))
           .build();
     }
 
