0ed7809567e7fe1093330b5c112a8eb4669a5bae
Poe Poe <poepoe88@gmail.com>
2015-03-25 15:04:48 +0800

    systembartint color for detail screen

--- app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/SentenceActivity.java
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/ui/SentenceActivity.java
@@ -1,15 +1,32 @@
 package com.poepoemyintswe.weeklyenglish.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import com.poepoemyintswe.weeklyenglish.R;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 public class SentenceActivity extends BaseActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    String[] colors = getPrimaryColor();
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      SystemBarTintManager tintManager = new SystemBarTintManager(this);
+      tintManager.setStatusBarTintEnabled(true);
+      tintManager.setNavigationBarTintEnabled(true);
+      tintManager.setTintColor(Color.parseColor(colors[1]));
+    } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+      Window window = getWindow();
+      window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+      window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+      window.setStatusBarColor(Color.parseColor(colors[1]));
+    }
   }
 
   @Override protected int getLayoutResource() {
