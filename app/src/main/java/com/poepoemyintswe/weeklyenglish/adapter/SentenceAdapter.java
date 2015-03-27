51cdb8988d2998210bd457523b3e40b3b775ea92
Poe Poe <poepoe88@gmail.com>
2015-03-27 20:51:32 +0800

    Sentence Adapter and Layout

--- app/src/main/java/com/poepoemyintswe/weeklyenglish/adapter/SentenceAdapter.java
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/adapter/SentenceAdapter.java
@@ -1,5 +1,7 @@
 package com.poepoemyintswe.weeklyenglish.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -12,19 +14,38 @@ import com.poepoemyintswe.weeklyenglish.db.Sentence;
  * Created by poepoe on 27/3/15.
  */
 public class SentenceAdapter extends RealmAdapter<Sentence, SentenceAdapter.ViewHolder> {
+
+  private final LayoutInflater inflater;
+  private final OnItemClickListener listener;
+
+  public SentenceAdapter(Context context, OnItemClickListener listener) {
+    this.inflater = LayoutInflater.from(context);
+    this.listener = listener;
+  }
+
   @Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return null;
+    return new ViewHolder(inflater.inflate(R.layout.row_sentence, parent, false));
   }
 
   @Override public void onBindViewHolder(ViewHolder holder, int position) {
+    final Sentence sentence = getItem(position);
+    holder.eng.setText(sentence.getEnglish());
+    holder.my.setText(sentence.getMyanmar());
+    holder.itemView.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        listener.onItemClick(sentence);
+      }
+    });
+  }
 
+  public static interface OnItemClickListener {
+    public void onItemClick(Sentence sentence);
   }
 
   public static class ViewHolder extends RealmAdapter.ViewHolder {
 
-    @InjectView(R.id.id) TextView id;
-    @InjectView(R.id.title) TextView title;
-    @InjectView(R.id.explanation) TextView explanation;
+    @InjectView(R.id.eng_sentence) TextView eng;
+    @InjectView(R.id.my_sentence) TextView my;
 
     public ViewHolder(View itemView) {
       super(itemView);
