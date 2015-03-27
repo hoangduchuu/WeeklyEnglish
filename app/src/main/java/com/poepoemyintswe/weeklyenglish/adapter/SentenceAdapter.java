d721e42b452d3fd36d22f311cd1460b6df1c6d7c
Poe Poe <poepoe88@gmail.com>
2015-03-27 20:44:57 +0800

    sentence adapter calss

--- app/src/main/java/com/poepoemyintswe/weeklyenglish/adapter/SentenceAdapter.java
+++ app/src/main/java/com/poepoemyintswe/weeklyenglish/adapter/SentenceAdapter.java
@@ -1,7 +1,34 @@
 package com.poepoemyintswe.weeklyenglish.adapter;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.poepoemyintswe.weeklyenglish.R;
+import com.poepoemyintswe.weeklyenglish.db.Sentence;
+
 /**
  * Created by poepoe on 27/3/15.
  */
-public class SentenceAdapter {
+public class SentenceAdapter extends RealmAdapter<Sentence, SentenceAdapter.ViewHolder> {
+  @Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    return null;
+  }
+
+  @Override public void onBindViewHolder(ViewHolder holder, int position) {
+
+  }
+
+  public static class ViewHolder extends RealmAdapter.ViewHolder {
+
+    @InjectView(R.id.id) TextView id;
+    @InjectView(R.id.title) TextView title;
+    @InjectView(R.id.explanation) TextView explanation;
+
+    public ViewHolder(View itemView) {
+      super(itemView);
+      ButterKnife.inject(this, itemView);
+    }
+  }
 }
